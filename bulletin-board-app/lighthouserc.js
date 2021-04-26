2cdb6dfa980380a56fda91124c9dc7d3c4e2203b
Gabriel Starczewski <38763186+gabrielstar@users.noreply.github.com>
2021-04-26 15:33:37 +0200

    Update lighthouserc.js

--- bulletin-board-app/lighthouserc.js
+++ bulletin-board-app/lighthouserc.js
@@ -2,9 +2,8 @@ module.exports = {
     ci: {
         collect: {
             isSinglePageApplication: true,
-			startServerCommand : 'while [ 1==1 ]; do echo "listen";sleep 1; done',
-			startServerReadyPattern : 'listen',
-			puppeteerScript: '../puppeteer/pagePWC.js', 
+	    startServerReadyPattern : 'magic',
+	    puppeteerScript: '../puppeteer/pagePWC.js', 
             url: ['https://www.pwc.com/'] //- in the pipeline
             //url : ['https://appxx.azurewebsites.net/'] //or against a deployed application
         },
